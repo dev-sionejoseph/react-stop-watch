cfafcb3ec7f991c87835648e3ea78d011546664e
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-03-28 22:27:12 -0500

    Edited Timer Component

--- stop-watch/src/App.js
+++ stop-watch/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Timer from './components/Timer'
 
 function App() {
   return (
     <div className="App">
-      
+      <Timer />
     </div>
   );
 }
